4ac1995dc6ccc735a137655d2325aca6c747303e
Monika Karanovic <monikakaranovic7@gmail.com>
2020-04-07 23:11:17 -0400

    final push

---
+++ src/resumeData.js
@@ -0,0 +1,111 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Monika Karanovic",
+    "role": "Full Stack Developer",
+    "linkedinId":"https://www.linkedin.com",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like to learn about new technologies in my free time and help people resolve technical problems.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Monika933",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"skype",
+          "url":"https://stackoverflow.com/users/12288827/monika",
+          "className":"fa fa-stack-overflow"
+        }
+      ],
+    "aboutme":"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged",
+    "address":"Virginia, USA",
+    "website":"https://monika933.github.io/UpdatedPortfolio/",
+    "education":[
+      {
+        "UniversityName":"GW University",
+        // "specialization":"Some specialization",
+         "MonthOfPassing":"April",
+         "YearOfPassing":"2020",
+        // "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"Union University of Law",
+        // "specialization":"Some specialization",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2016",
+        // "Achievements":"Some Achievements"
+      }
+    ],
+    // "work":[
+    //   {
+    //     "CompanyName":"Some Company",
+    //     "specialization":"Some specialization",
+    //     "MonthOfLeaving":"Jan",
+    //     "YearOfLeaving":"2018",
+    //     "Achievements":"Some Achievements"
+    //   },
+    //   {
+    //     "CompanyName":"Some Company",
+    //     "specialization":"Some specialization",
+    //     "MonthOfLeaving":"Jan",
+    //     "YearOfLeaving":"2018",
+    //     "Achievements":"Some Achievements"
+    //   }
+    // ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"New York app",
+        "imgurl":"images/newYork.JPG",
+        "url":"https://wgbcamp.github.io/Project1/",
+      },
+      {
+        "name":"project2",
+        "description":"Spa Relax",
+        "imgurl":"images/spaRelax.jpg",
+        "url":"https://project2-spa.herokuapp.com/",
+      },
+      {
+        "name":"project3",
+        "description":"Budget app",  
+        "imgurl":"images/budget-tracker.jpg",
+        "url":"https://offline-budget-application.herokuapp.com/",
+
+      },
+      {
+        "name":"project4",
+        "description":"weather",
+        "imgurl":"images/weatherDashboard.jpg",
+        "url":"https://monika933.github.io/WeatherDashboard/",
+      }
+    ],
+    // "testimonials":[
+    //   {
+    //     // "description":"This is a sample testimonial",
+    //     // "name":"Some technical guy"
+    //   },
+    //   {
+    //     "description":"This is a sample testimonial",
+    //     "name":"Some technical guy"
+    //   }
+    // ]
+  }
+  
+  export default resumeData
